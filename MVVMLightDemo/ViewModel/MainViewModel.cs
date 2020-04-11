6d53117580af537c79e73ccf9d804b98cdda33c1
Chamara Dodandeniya <92chamara@gmail.com>
2020-04-11 09:38:59 +0530

    First commit without jenkins file

---
+++ MVVMLightDemo/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+using System;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace MVVMLightDemo.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string textVal = "Change me";
+
+        public MainViewModel()
+        {
+            OnTextChangeButtonClickedCommand = new RelayCommand(OnTextChangeButtonClickedHandler);
+        }
+
+        public string TextVal
+        {
+            get { return textVal; }
+            set { Set(() => TextVal, ref textVal, value); }
+        }
+
+        public RelayCommand OnTextChangeButtonClickedCommand { get; private set; }
+
+        private void OnTextChangeButtonClickedHandler()
+        {
+            TextVal = "Hello MVVM light.";
+        }
+    }
+}
